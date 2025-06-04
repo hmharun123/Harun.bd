245000caa9ce2b0112c57d6e554ee7cbe3f0e842
hmharun123 <mdh239003@gmail.com>
2025-06-04 13:27:15 +0600

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,1 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "your_harunrm900@gmail.com"; 
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
+    $subject = "New Message from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
